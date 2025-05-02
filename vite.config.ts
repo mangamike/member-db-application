3337a8c3879410fb427bc56302b0dd17893f429c
mikeosexton <mangamike@gmail.com>
2025-05-01 20:09:32 -0400

    Working L12 V3 and Docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
-import tailwindcss from "@tailwindcss/vite";
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
@@ -9,10 +8,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/js/app.ts', 'resources/css/app.css'],
-            ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
-        tailwindcss(),
         vue({
             template: {
                 transformAssetUrls: {
